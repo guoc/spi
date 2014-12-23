89184e8bf468413e405f5c68a7e36cfa2ff60fd4
guoc <guochen42+dev@gmail.com>
2014-12-23 19:12:39 +1100

    Update the script to make sure all changes are commited before release build.

--- check_no_changes_after_last_commit.sh
+++ check_no_changes_after_last_commit.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# If build for release, this script makes sure no changes after last commit.
+
 releaseConfig="Release"
 if [ "$releaseConfig" = "${CONFIGURATION}" ] && [[ -n $(git status -s) ]]; then
   echo "Commit is required before archive!"
