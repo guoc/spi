07ef5fac18714d54eba00e882c0feb093a0de1fc
guoc <guochen42+dev@gmail.com>
2014-12-25 01:24:24 +1100

    Test candidates.sqlite exists first in update_candidates_database_if_necessary.py

--- update_candidates_database_if_necessary.py
+++ update_candidates_database_if_necessary.py
@@ -7,6 +7,7 @@ import subprocess
 
 candidatesFiles = os.listdir('./Candidates')
 
-for candidatesFile in candidatesFiles:
-  if os.path.getmtime('./Candidates/' + candidatesFile) > os.path.getmtime("./SPiKeyboard/candidates.sqlite"):
-    subprocess.call(['./update_candidates_database.sh'])
+if os.path.isfile("./SPiKeyboard/candidates.sqlite"):
+  for candidatesFile in candidatesFiles:
+    if os.path.getmtime('./Candidates/' + candidatesFile) > os.path.getmtime("./SPiKeyboard/candidates.sqlite"):
+      subprocess.call(['./update_candidates_database.sh'])
