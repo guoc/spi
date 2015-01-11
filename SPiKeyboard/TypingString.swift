95127e93b6620147e9b477cc4b5c087cae442aaf
guoc <guochen42+dev@gmail.com>
2015-01-11 16:09:09 +1100

    Fix: Add wrong input history
    e.g. if support@spiim.com has been recorded, then typing support@ and select support@spiim.com will add new input history(support@: support@)

--- SPiKeyboard/TypingString.swift
+++ SPiKeyboard/TypingString.swift
@@ -158,9 +158,7 @@ class TypingString {
     }
     
     func getCandidate() -> Candidate {
-        if userTypingString.containsNonLetters() == true {
-            return Candidate(text: userTypingString, withSpecialString: userTypingString)
-        } else if typeOfRemainingFormalizedTyping == .Empty && _cachedCandidatesWithCorrespondingTyping.isEmpty == false {
+        if typeOfRemainingFormalizedTyping == .Empty && _cachedCandidatesWithCorrespondingTyping.isEmpty == false {
             return Candidate(text: "".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.text})), withShuangpinString: " ".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.queryCode})))
         } else if hasSelectedPartialCandidates() {
             return Candidate(text: "".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.text})))
