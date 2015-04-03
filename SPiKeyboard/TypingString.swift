guoc/spi

commit bcc7ab3b6efba338bd0ca385ddd8792e4612a80a

Fix logic in TypingString

SPi/Info.plist
@@ -19,7 +19,7 @@
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>198</string>
+	<string>199</string>
 	<key>LSRequiresIPhoneOS</key>
 	<true/>
 	<key>UILaunchStoryboardName</key>

SPiKeyboard/Info.plist
@@ -21,7 +21,7 @@
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>198</string>
+	<string>199</string>
 	<key>NSExtension</key>
 	<dict>
 		<key>NSExtensionAttributes</key>

SPiKeyboard/TypingString.swift
@@ -158,7 +158,7 @@ class TypingString {
     }
     
     func getCandidate() -> Candidate {
-        if typeOfRemainingFormalizedTyping == .Empty && _cachedCandidatesWithCorrespondingTyping.isEmpty == false {
+        if typeOfRemainingFormalizedTyping == .Empty && _cachedCandidatesWithCorrespondingTyping.count > 1 {
             return Candidate(text: "".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.text})), withShuangpinString: " ".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.queryCode})))
         } else if hasSelectedPartialCandidates() {
             return Candidate(text: "".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.text})))

commit 7df28aa564ef30b75dfa7e07fd8697f33ccde13d

Reverse to previous version in TypingString to fix the issue introduced by bcc7ab3b.

SPi/Info.plist
@@ -19,7 +19,7 @@
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>201</string>
+	<string>202</string>
 	<key>LSRequiresIPhoneOS</key>
 	<true/>
 	<key>UILaunchStoryboardName</key>

SPiKeyboard/Info.plist
@@ -21,7 +21,7 @@
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>201</string>
+	<string>202</string>
 	<key>NSExtension</key>
 	<dict>
 		<key>NSExtensionAttributes</key>

SPiKeyboard/TypingString.swift
@@ -158,7 +158,7 @@ class TypingString {
     }
     
     func getCandidate() -> Candidate {
-        if typeOfRemainingFormalizedTyping == .Empty && _cachedCandidatesWithCorrespondingTyping.count > 1 {
+        if typeOfRemainingFormalizedTyping == .Empty && _cachedCandidatesWithCorrespondingTyping.isEmpty == false {
             return Candidate(text: "".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.text})), withShuangpinString: " ".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.queryCode})))
         } else if hasSelectedPartialCandidates() {
             return Candidate(text: "".join(_cachedCandidatesWithCorrespondingTyping.map({x -> String in return x.candidate.text})))